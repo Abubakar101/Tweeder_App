1e5497539e383ffa687664b4d5c8033f46ba83c7
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-09-16 15:25:55 -0400

    axios  installed and some fixes in app.js

--- tweedr/client/src/App.js
+++ tweedr/client/src/App.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react';
 import TweedrFeed from './components/TweedrFeed';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
+import Input from './components/Input';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      data: [],
+      inputField: 0,
+      timeField: ''
+    }
+
+    this.handleInputListener = this.handleInputListener.bind(this)
+    this.handleSubmitListener = this.handleSubmitListener.bind(this)
+  }
+
+  componentDidMount() {
+    axios('/api/tweeds')
+    .then(res => {
+      console.log(res.data.tweeds);
+        this.setState({
+            data: res.data.tweeds
+        })
+    })
+  }
+
+  handleInputListener(event) {
+    this.setState({
+      inputField: event.target.value,
+    })
+  }
+
+  handleSubmitListener(event) {
+      event.preventDefault();
+      event.target.content='';
+
+      this.setState({
+        timeField: event.targer.time
+      })
+
+      axios.post('/api/tweeds' ,  {
+        tweed_text: this.state.inputField,
+        tweed_time: this.state.time
+      }).then(res => {
+        if(res.data.quote.id !== undefined) {
+            const newTweed = {
+              tweed_text: res.data.quote.tweed_text,
+              tweed_time: res.data.quote.tweed_time
+          }
+
+          this.setState(prevState => {
+            return {
+              quotes: prevState.quotes.concat(newQuote)
+            };
+          });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <TweedrFeed data />
       </div>
     );
   }
