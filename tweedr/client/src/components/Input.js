786a917993edadf696d51bd600c38d364e44c10d
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2017-09-16 11:44:55 -0400

    added a form for the tweet

--- tweedr/client/src/components/Input.js
+++ tweedr/client/src/components/Input.js
@@ -1,1 +1,19 @@
-Input.js
+import React, { Component } from 'react';
+const InputFrom = (props) => {
+  return(
+    <form className="inputForm"
+      onSubmit={props.handleSubmit}
+      >
+      <input
+       type="text"
+       value={props.inputField}
+       name="tweed_text"
+       placeholder="Tweet Here"
+       onchange={props.handleChange}
+       />
+      <br/>
+      <button type="submit" value="submit">Tweet</button>
+    </form>
+    );
+}
+export default InputForm;
